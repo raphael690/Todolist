fdd118d81282379255436f444b186e005872db25
Raphael <raphael.tecnologiainformacaorj@gmail.com>
2021-08-24 02:02:42 -0300

    commit inicial

---
+++ app/src/main/java/br/com/aplicativo/todolist/extensions/AppExtensions.kt
@@ -0,0 +1,17 @@
+package br.com.aplicativo.todolist.extensions
+
+import com.google.android.material.textfield.TextInputLayout
+import java.text.SimpleDateFormat
+import java.util.*
+
+private val locale = Locale("pt", "BR")
+//formato  data dia, mes e ano
+fun Date.format() : String {
+    return SimpleDateFormat("dd/MM/yyyy", locale).format(this)
+}
+
+var TextInputLayout.text : String
+    get() = editText?.text?.toString() ?: ""
+    set(value) {
+        editText?.setText(value)
+    }
